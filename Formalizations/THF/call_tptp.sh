e8d4a8c0c728ec1974a0ee2c82b22e4a467e5768
Ceilican <bruno.wp@gmail.com>
2013-10-31 18:17:25 +0100

    Improve call_tptp script.

--- Formalizations/THF/call_tptp.sh
+++ Formalizations/THF/call_tptp.sh
@@ -4,17 +4,18 @@ call=./RemoteSOT.pl
 # install it and change the above paths accordingly
 
 proverlist='LEO-II---1.6.0 Satallax---2.7 Isabelle---2013 Nitrox---2013 agsyHOL---1.0 TPS---3.120601S1b'
-timeout=90
+timeout=60
 
 echo
 echo Asking various HOL-ATPs in Miami remotely \(thanks to Geoff Sutcliffe\)
 
 for file in "$@"
 do
+echo
     for prover in $proverlist 
     do
     function_to_fork () { 
-        echo $prover : $file ++++++ `$tptp4X -t $timeout -x $file | $call -t $timeout -s $prover | grep RESULT` 
+        echo $file ++++++ `$tptp4X -t $timeout -x $file | $call -t $timeout -s $prover | grep RESULT` 
     }
     function_to_fork &
     done
