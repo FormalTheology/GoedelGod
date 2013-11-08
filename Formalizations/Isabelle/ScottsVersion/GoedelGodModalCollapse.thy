6813b4ad8c58f45ce449c40d31a03dc2801f028d
Christoph Benzmueller iMac <c.benzmueller@gmail.com>
2013-11-08 21:04:37 +0100

    update of Isabelle/HOL files

--- Formalizations/Isabelle/ScottsVersion/GoedelGodModalCollapse.thy
+++ Formalizations/Isabelle/ScottsVersion/GoedelGodModalCollapse.thy
@@ -14,8 +14,6 @@ definitions and previous theorems are used by Satallax;
 hence it suggests to call Metis using everything, but this (unsurprisingly) fails.
 Attempting to use ``Sledegehammer min'' to minimize Sledgehammer's suggestion does not work.
 Nevertheless, calling Metis with T2, T3 and ess_def does work. *} 
-  
-ToDo: The universal quantifier below has wrong type.
 
   lemma MC: "[\<forall> (\<lambda>\<Phi>.( \<Phi> m\<Rightarrow> (\<box> \<Phi>)))]"
   using T2 T3 ess_def sym sledgehammer [provers = remote_satallax]
