4f368a290c82377da05978e6f68f6a3517ad7d91
Christoph Benzmueller iMac <c.benzmueller@gmail.com>
2013-11-10 10:12:12 +0100

    modifications of Isabelle/HOL files

--- Formalizations/Isabelle/ScottsVersion/GoedelGodExtensions.thy
+++ Formalizations/Isabelle/ScottsVersion/GoedelGodExtensions.thy
@@ -9,7 +9,7 @@ section {* Further results on G\"odel's God. *}
 
 text {* Lifted Leibniz equality is introduced. *}
 
-  abbreviation mequals :: "\<mu> \<Rightarrow> \<mu> \<Rightarrow> \<sigma>" (infixr "m=" 90) where "x m= y \<equiv> \<forall>(\<lambda> \<phi>.(\<phi> x m\<rightarrow> \<phi> y))" 
+  abbreviation mequals :: "'a \<Rightarrow> 'a \<Rightarrow> \<sigma>" (infixr "m=" 90) where "x m= y \<equiv> \<forall>(\<lambda> \<phi>.(\<phi> x m\<rightarrow> \<phi> y))" 
 
 text {* G\"odel's God is flawless, that is, he has only positive properties. *}
 
@@ -30,7 +30,34 @@ text {* Add-on: We briefly show that lifted Leibniz equality indeed denotes equa
   lemma eqtest2: "[x m= y] \<Longrightarrow> x = y"
   sledgehammer [provers = remote_satallax] oops
 
+text {* m= is an a fully extensional congruence relation. *}    
+  
+  lemma eqttest_refl : "[\<forall>(\<lambda>x. x m= x)]"
+  sledgehammer [provers = remote_leo2] by metis
+  
+  lemma eqttest_sym : "[\<forall>(\<lambda>x.\<forall>(\<lambda>y. x m= y m\<rightarrow> y m= x))]"
+  sledgehammer [provers = remote_satallax] oops
 
+  lemma eqttest_trans : "[\<forall>(\<lambda>x.\<forall>(\<lambda>y.\<forall>(\<lambda>z. (x m= y m\<and> y m= z) m\<rightarrow> x m= z)))]"
+  sledgehammer [provers = remote_leo2] by metis
+
+  lemma eqttest_congr : "[\<forall>(\<lambda>x.\<forall>(\<lambda>y.\<forall>(\<lambda>f. x m= y m\<rightarrow> (f x m= f y))))]"
+  sledgehammer [provers = remote_leo2] oops 
+  
+  lemma eqttest_funcExt_triv : "[\<forall>(\<lambda>f.\<forall>(\<lambda>g. f m= g m\<rightarrow> \<forall>(\<lambda>x. f x m\<rightarrow> g x)))]"
+  sledgehammer [provers = remote_leo2] oops
+
+  abbreviation mequiv:: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<equiv>" 73) where "\<phi> m\<equiv> \<psi> \<equiv> (\<lambda>w. \<phi> w \<longleftrightarrow> \<psi> w)"  
+  
+  lemma eqttest_boolext_triv : "[\<forall>(\<lambda>p.\<forall>(\<lambda>q. p m= q m\<rightarrow> p m\<equiv> q))]"
+  sledgehammer [provers = remote_leo2] oops
+
+  lemma eqttest_funcExt : "[\<forall>(\<lambda>f.\<forall>(\<lambda>g. \<forall>(\<lambda>x. f x m\<rightarrow> g x) m\<rightarrow> f m= g))]"
+  sledgehammer [provers = remote_leo2] by metis  
+  
+  lemma eqttest_boolext : "[\<forall>(\<lambda>p.\<forall>(\<lambda>q. p m\<equiv> q m\<rightarrow> p m= q))]"
+  sledgehammer [provers = remote_leo2] by metis
+  
 (*<*) 
 end
 (*>*) 
