c7ae45ebac2b76c0b52fbc199e9fcabf6e956344
Christoph Benzmueller iMac <c.benzmueller@gmail.com>
2013-11-11 10:12:36 +0100

    further update of ScottsVersion in Isabelle/HOL

--- Formalizations/Isabelle/ScottsVersion/GoedelGodModalCollapse.thy
+++ Formalizations/Isabelle/ScottsVersion/GoedelGodModalCollapse.thy
@@ -13,7 +13,7 @@ However, sledgehammer does not seem to be able to determine which axioms,
 definitions and previous theorems are used by Satallax;
 hence it suggests to call Metis using everything, but this (unsurprisingly) fails.
 Attempting to use `Sledegehammer min' to minimize Sledgehammer's suggestion does not work.
-Nevertheless, calling Metis with @{text "T2"}, @{text "T3} and @{text "ess_def"} does work. *} 
+Nevertheless, calling Metis with @{text "T2"}, @{text "T3"} and @{text "ess_def"} does work. *} 
 
   lemma MC: "[\<forall>(\<lambda>\<phi>.(\<phi> m\<rightarrow> (\<box> \<phi>)))]"
   using T2 T3 ess_def sym sledgehammer [provers = remote_satallax]
