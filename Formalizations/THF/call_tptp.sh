f607c945267f20ceb6e6ee23ad3f9a3d06481e22
Christoph Benzmueller iMac <c.benzmueller@gmail.com>
2014-08-15 12:52:44 +0200

    small update

--- Formalizations/THF/call_tptp.sh
+++ Formalizations/THF/call_tptp.sh
@@ -4,15 +4,25 @@ call=./RemoteSOT.pl
 # install it and change the above paths accordingly
 
 #proverlist='LEO-II---1.6.2 Satallax---2.7 Isabelle---2013 Nitrox---2013 agsyHOL---1.0 TPS---3.120601S1b'
-proverlist='LEO-II---1.6.2 Satallax---2.7 Nitrox---2013'
+#proverlist='LEO-II---1.6.2 Satallax---2.7 Nitrox---2013'
+proverlist='LEO-II---1.6.2 Satallax-MaLeS---1.2 Nitrox---2013'
+
 timeout=60
+
+suffix=$(date "+%Y-%m-%d--%H:%M:%S")
+resultsfile=RESULTS-$suffix
+
+touch $resultsfile
+rm $resultsfile
 
 echo
 echo Asking various HOL-ATPs in Miami remotely \(thanks to Geoff Sutcliffe\)
+echo Asking various HOL-ATPs in Miami remotely \(thanks to Geoff Sutcliffe\) >> $resultsfile
 
 for file in "$@"
 do
 echo
+echo >> $resultsfile
     for prover in $proverlist 
     do
     function_to_fork () { 
@@ -20,12 +30,14 @@ echo
       while ! (echo $res | grep --quiet RESULT); do
           res=`$tptp4X -t $timeout -x $file | $call -t $timeout -s $prover |grep RESULT` 
 	  echo ${file}---${prover}---${res}
+	  echo ${file}---${prover}---${res} >> $resultsfile
       done   
     }
     function_to_fork &
     done
     wait
     echo Done 
+    echo Done >> $resultsfile
 done
 
 
