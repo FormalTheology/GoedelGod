6f166f8a1f95ad3ee1545e79166c5f4ffd8a714a
cbenzmueller <c.benzmueller@gmail.com>
2014-08-21 14:57:36 +0200

    added Isabelle version of Bjordal's paper

---
+++ Formalizations/Isabelle/BjordalVersion/Bjordal_C.thy
@@ -0,0 +1,104 @@
+(*<*) 
+theory Bjordal_C
+imports Main 
+
+begin
+(*>*)
+
+section {* Introduction *}
+
+text {* We verify Frode Bjordal's paper from 1998. 
+In this file Bjordal_C.thy we check the claim whether
+his definitions for MCP and N together with his two axioms 
+Ax1 and Ax2 imply necessary existence of God. *}
+
+section {* An Embedding of QML KB in HOL *}
+
+text {* The types @{text "i"} for possible worlds and $\mu$ for individuals are introduced. *}
+
+  typedecl i    -- "the type for possible worlds" 
+  typedecl \<mu>    -- "the type for indiviuals"      
+
+text {* Possible worlds are connected by an accessibility relation @{text "r"}.*} 
+
+  consts r :: "i \<Rightarrow> i \<Rightarrow> bool" (infixr "r" 70)    -- "accessibility relation r"   
+
+text {* QML formulas are translated as HOL terms of type @{typ "i \<Rightarrow> bool"}. 
+This type is abbreviated as @{text "\<sigma>"}. *}
+
+  type_synonym \<sigma> = "(i \<Rightarrow> bool)"
+ 
+text {* The classical connectives $\neg, \wedge, \rightarrow$, and $\forall$
+(over individuals and over sets of individuals) and $\exists$ (over individuals) are
+lifted to type $\sigma$. The lifted connectives are @{text "m\<not>"}, @{text "m\<and>"}, @{text "m\<rightarrow>"},
+@{text "\<forall>"}, and @{text "\<exists>"} (the latter two are modeled as constant symbols). 
+Other connectives can be introduced analogously. We exemplarily do this for @{text "m\<or>"} , 
+@{text "m\<equiv>"}, and @{text "mL="} (Leibniz equality on individuals). Moreover, the modal 
+operators @{text "\<box>"} and @{text "\<diamond>"}  are introduced. Definitions could be used instead of 
+abbreviations. *}
+
+  abbreviation mnot :: "\<sigma> \<Rightarrow> \<sigma>" ("m\<not>") where "m\<not> \<phi> \<equiv> (\<lambda>w. \<not> \<phi> w)"    
+  abbreviation mand :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<and>" 51) where "\<phi> m\<and> \<psi> \<equiv> (\<lambda>w. \<phi> w \<and> \<psi> w)"   
+  abbreviation mor :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<or>" 50) where "\<phi> m\<or> \<psi> \<equiv> (\<lambda>w. \<phi> w \<or> \<psi> w)"   
+  abbreviation mimplies :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<rightarrow>" 49) where "\<phi> m\<rightarrow> \<psi> \<equiv> (\<lambda>w. \<phi> w \<longrightarrow> \<psi> w)"  
+  abbreviation mequiv:: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<equiv>" 48) where "\<phi> m\<equiv> \<psi> \<equiv> (\<lambda>w. \<phi> w \<longleftrightarrow> \<psi> w)"  
+  abbreviation mforall :: "('a \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<forall>") where "\<forall> \<Phi> \<equiv> (\<lambda>w. \<forall>x. \<Phi> x w)"   
+  abbreviation mexists :: "('a \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<exists>") where "\<exists> \<Phi> \<equiv> (\<lambda>w. \<exists>x. \<Phi> x w)"
+  abbreviation mLeibeq :: "\<mu> \<Rightarrow> \<mu> \<Rightarrow> \<sigma>" (infixr "mL=" 52) where "x mL= y \<equiv> \<forall>(\<lambda>\<phi>. (\<phi> x m\<rightarrow> \<phi> y))"
+  abbreviation mbox :: "\<sigma> \<Rightarrow> \<sigma>" ("\<box>") where "\<box> \<phi> \<equiv> (\<lambda>w. \<forall>v.  w r v \<longrightarrow> \<phi> v)"
+  abbreviation mdia :: "\<sigma> \<Rightarrow> \<sigma>" ("\<diamond>") where "\<diamond> \<phi> \<equiv> (\<lambda>w. \<exists>v. w r v \<and> \<phi> v)" 
+  
+text {* For grounding lifted formulas, the meta-predicate @{text "valid"} is introduced. *}
+
+  (*<*) no_syntax "_list" :: "args \<Rightarrow> 'a list" ("[(_)]") (*>*) 
+  abbreviation valid :: "\<sigma> \<Rightarrow> bool" ("[_]") where "[p] \<equiv> \<forall>w. p w"
+  
+section {* Bjordal's argument *}  
+ 
+text {* Constant symbol @{text "P"} (for 'Positive') is introduced
+here as a primitive constant. Then G is defined using P. This is
+Gödel's definition D1. *}  
+
+text {* Constant symbol @{text "G"} (for 'God') is introduced
+here as a primitive constant. Then P is defined using G. This
+is Bjordal's definition D. *}
+
+ consts G :: "\<mu> \<Rightarrow> \<sigma>"   
+ definition P :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>"  
+ where "P = (\<lambda>\<Phi>. \<box>(\<forall>(\<lambda>x. G x m\<rightarrow> \<Phi> x)))" 
+
+text {* We introduce Bjordal's definitions MCP and N. *}
+ 
+ definition MCP :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<mu> \<Rightarrow> \<sigma>"  
+ where "MCP = (\<lambda>\<Phi> x. \<Phi> x m\<and> P \<Phi> m\<and> 
+   \<forall>(\<lambda>\<Psi>. (\<Psi> x m\<and> P \<Psi>) m\<rightarrow> \<box> (\<forall>(\<lambda>y. \<Phi> y m\<rightarrow> \<Psi> y))))"
+
+ definition N :: "\<mu> \<Rightarrow> \<sigma>"  
+ where "N = (\<lambda>x. \<forall>(\<lambda>\<Phi>. MCP \<Phi> x m\<rightarrow> \<box> (\<exists>(\<lambda>y. \<Phi> y))))"  
+
+text {* We postulate Bjordal's axioms Ax1 and Ax2. *}
+
+ axiomatization where
+  Ax1: "[\<forall>(\<lambda>\<Phi>. P \<Phi> m\<rightarrow> m\<not> (P (\<lambda>x. m\<not> (\<Phi> x))))]" and
+  Ax2: "[P N]"
+
+text {* We add axiom B (symmetry). *}
+
+ axiomatization where sym: "x r y \<longrightarrow> y r x" 
+
+text {* We prove necessarily God exists. *}
+ 
+ theorem T3: "[\<box> (\<exists> G)]" 
+ (* nitpick [user_axioms = true] *)
+ (* sledgehammer [provers = remote_leo2 remote_satallax] *)
+ by (metis Ax1 Ax2 Bjordal_C.sym MCP_def N_def P_def) 
+
+text {* Nitpick generates a countermodel to Modal Collapse. *}
+
+ lemma MC: "[\<forall>(\<lambda>\<Phi>.(\<Phi> m\<rightarrow> (\<box> \<Phi>)))]"  
+ nitpick [user_axioms = true]
+ oops
+
+(*<*) 
+end
+(*>*) 
