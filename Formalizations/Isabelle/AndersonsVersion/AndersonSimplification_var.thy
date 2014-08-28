b80b34f10f7647e467d6bf2fe340ea4939af1e91
Ceilican <bruno.wp@gmail.com>
2014-08-28 15:58:08 +0200

    Add Isabelle verification of Anderson's proposed simplification

---
+++ Formalizations/Isabelle/AndersonsVersion/AndersonSimplification_var.thy
@@ -0,0 +1,85 @@
+(*<*) 
+theory AndersonSimplification_var
+imports Main 
+
+begin
+(*>*)
+
+section {* An Embedding of QML S5 in HOL *}
+
+text {* The types @{text "i"} for possible worlds and $\mu$ for individuals are introduced. *}
+
+  typedecl i    -- "the type for possible worlds" 
+  typedecl \<mu>    -- "the type for indiviuals"      
+
+text {* Possible worlds are connected by an accessibility relation @{text "r"}.*} 
+
+  consts r :: "i \<Rightarrow> i \<Rightarrow> bool" (infixr "r" 70)    -- "accessibility relation r"   
+
+text {* QML formulas are translated as HOL terms of type @{typ "i \<Rightarrow> bool"}. 
+This type is abbreviated as @{text "\<sigma>"}. *}
+
+  type_synonym \<sigma> = "(i \<Rightarrow> bool)"
+ 
+text {* The classical connectives $\neg, \wedge, \rightarrow$, and $\forall$
+(over individuals and over sets of individuals) and $\exists$ (over individuals) are
+lifted to type $\sigma$. The lifted connectives are @{text "m\<not>"}, @{text "m\<and>"}, @{text "m\<rightarrow>"},
+@{text "\<forall>"}, and @{text "\<exists>"} (the latter two are modeled as constant symbols). 
+Other connectives can be introduced analogously. We exemplarily do this for @{text "m\<or>"} , 
+@{text "m\<equiv>"}, and @{text "mL="} (Leibniz equality on individuals). Moreover, the modal 
+operators @{text "\<box>"} and @{text "\<diamond>"}  are introduced. Definitions could be used instead of 
+abbreviations. *}
+
+  consts eiw :: "\<mu> \<Rightarrow> i \<Rightarrow> bool"
+  axiomatization where nonempty: "\<forall>w. \<exists>x. eiw x w"
+
+  abbreviation mnot :: "\<sigma> \<Rightarrow> \<sigma>" ("m\<not>") where "m\<not> \<phi> \<equiv> (\<lambda>w. \<not> \<phi> w)"    
+  abbreviation mand :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<and>" 51) where "\<phi> m\<and> \<psi> \<equiv> (\<lambda>w. \<phi> w \<and> \<psi> w)"   
+  abbreviation mor :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<or>" 50) where "\<phi> m\<or> \<psi> \<equiv> (\<lambda>w. \<phi> w \<or> \<psi> w)"   
+  abbreviation mimplies :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<rightarrow>" 49) where "\<phi> m\<rightarrow> \<psi> \<equiv> (\<lambda>w. \<phi> w \<longrightarrow> \<psi> w)"  
+  abbreviation mequiv:: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<equiv>" 48) where "\<phi> m\<equiv> \<psi> \<equiv> (\<lambda>w. \<phi> w \<longleftrightarrow> \<psi> w)"  
+  abbreviation mforalle :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<forall>e") where "\<forall>e \<Phi> \<equiv> (\<lambda>w. \<forall>x. (eiw x w) \<longrightarrow> (\<Phi> x w))"     
+  abbreviation mexistse :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<exists>e") where "\<exists>e \<Phi> \<equiv> (\<lambda>w. \<exists>x. (eiw x w) \<and> \<Phi> x w)" 
+  abbreviation mforall :: "('a \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<forall>") where "\<forall> \<Phi> \<equiv> (\<lambda>w. \<forall>x. \<Phi> x w)"   
+  abbreviation mexists :: "('a \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<exists>") where "\<exists> \<Phi> \<equiv> (\<lambda>w. \<exists>x. \<Phi> x w)"
+  abbreviation mLeibeq :: "\<mu> \<Rightarrow> \<mu> \<Rightarrow> \<sigma>" (infixr "mL=" 52) where "x mL= y \<equiv> \<forall>(\<lambda>\<phi>. (\<phi> x m\<rightarrow> \<phi> y))"
+  abbreviation mbox :: "\<sigma> \<Rightarrow> \<sigma>" ("\<box>") where "\<box> \<phi> \<equiv> (\<lambda>w. \<forall>v.  w r v \<longrightarrow> \<phi> v)"
+  abbreviation mdia :: "\<sigma> \<Rightarrow> \<sigma>" ("\<diamond>") where "\<diamond> \<phi> \<equiv> (\<lambda>w. \<exists>v. w r v \<and> \<phi> v)" 
+
+  axiomatization where 
+  sym: "x r y \<longrightarrow> y r x" and
+  refl: "x r x" and
+  trans: "((x r y) \<and> (y r z)) \<longrightarrow> (x r z)"
+
+
+ 
+text {* For grounding lifted formulas, the meta-predicate @{text "valid"} is introduced. *}
+
+  (*<*) no_syntax "_list" :: "args \<Rightarrow> 'a list" ("[(_)]") (*>*) 
+  abbreviation valid :: "\<sigma> \<Rightarrow> bool" ("[_]") where "[p] \<equiv> \<forall>w. p w"
+  
+
+section {* Anderson's Simplification *}   
+  
+  text {* Anderson 1990 claims (in page 7) that axioms A1, A2 and A4 are provable,
+          as long as "Positive" is defined in a certain way based on a new constant "Defective".
+          These claims are confirmed here.
+          However, proving A4 requires symmetry and transitivity of the accessibility relation *} 
+
+  consts Defective :: "\<mu> \<Rightarrow> \<sigma>" 
+  
+  definition P :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" where
+  "P = (\<lambda>\<Phi>. (\<box> (\<forall>e(\<lambda>x. (m\<not> (\<Phi>(x))) m\<rightarrow> Defective(x)))) m\<and> (m\<not> (\<box>(\<forall>e(\<lambda>x. (\<Phi>(x)) m\<rightarrow> Defective(x))) )))"
+
+  theorem A1:  "[\<forall>(\<lambda>\<Phi>. ((P \<Phi>) m\<rightarrow> m\<not> (P (\<lambda>x. m\<not> (\<Phi> x))) )  )]"
+  by (metis P_def)
+
+  theorem A2:  "[\<forall>(\<lambda>\<Phi>. \<forall>(\<lambda>\<Psi>. ( (P \<Phi>) m\<and> \<box> (\<forall>e(\<lambda>x. (\<Phi> x) m\<rightarrow> (\<Psi> x) ))) m\<rightarrow> (P \<Psi>)))]"
+  by (metis P_def)
+
+  theorem  A4:  "[\<forall>(\<lambda>\<Phi>. (P \<Phi>) m\<rightarrow> \<box> (P \<Phi>))]"
+  by (metis P_def sym trans)
+
+(*<*) 
+end
+(*>*) 
