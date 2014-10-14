cda31b8d521cd2338c74f934f48cecc9f65d6bed
cbenzmueller <c.benzmueller@fu-berlin.de>
2014-10-14 15:32:17 +0200

    file added

---
+++ Formalizations/Isabelle/Goedel/GoedelGodWithoutConjunctInEssVarying.thy
@@ -0,0 +1,146 @@
+(*<*) 
+theory GoedelGodWithoutConjunctInEssVarying
+imports Main 
+
+begin
+(*>*)
+
+section {* Introduction *}
+
+ text {* Dana Scott's version without the first conjunct in the definition of 
+         essence 
+*}
+
+  typedecl i    -- "the type for possible worlds" 
+  typedecl \<mu>    -- "the type for indiviuals"      
+
+  consts r :: "i \<Rightarrow> i \<Rightarrow> bool" (infixr "r" 70)    -- "accessibility relation r"   
+
+  type_synonym \<sigma> = "(i \<Rightarrow> bool)"
+
+  abbreviation mnot :: "\<sigma> \<Rightarrow> \<sigma>" ("m\<not>") where "m\<not> \<phi> \<equiv> (\<lambda>w. \<not> \<phi> w)"    
+  abbreviation mand :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<and>" 51) where "\<phi> m\<and> \<psi> \<equiv> (\<lambda>w. \<phi> w \<and> \<psi> w)"   
+  abbreviation mor :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<or>" 50) where "\<phi> m\<or> \<psi> \<equiv> (\<lambda>w. \<phi> w \<or> \<psi> w)"   
+  abbreviation mimplies :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<rightarrow>" 49) where "\<phi> m\<rightarrow> \<psi> \<equiv> (\<lambda>w. \<phi> w \<longrightarrow> \<psi> w)"  
+  abbreviation mequiv:: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (infixr "m\<equiv>" 48) where "\<phi> m\<equiv> \<psi> \<equiv> (\<lambda>w. \<phi> w \<longleftrightarrow> \<psi> w)"  
+  consts eiw :: "i \<Rightarrow> \<mu> \<Rightarrow> bool"  
+  axiomatization where nonempty: "\<forall>w. \<exists>x. eiw w x"
+  abbreviation mforall_ind :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<forall>i") where "\<forall>i P \<equiv> (\<lambda>w. \<forall>x. eiw w x \<longrightarrow> P x w)"
+  abbreviation mexists_ind :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<exists>i") where "\<exists>i P \<equiv> (\<lambda>w. \<exists>x. eiw w x \<and> P x w)"
+  abbreviation mforall :: "('a \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<forall>") where "\<forall> \<Phi> \<equiv> (\<lambda>w. \<forall>x. \<Phi> x w)"   
+  abbreviation mexists :: "('a \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" ("\<exists>") where "\<exists> \<Phi> \<equiv> (\<lambda>w. \<exists>x. \<Phi> x w)"
+  abbreviation mLeibeq :: "\<mu> \<Rightarrow> \<mu> \<Rightarrow> \<sigma>" (infixr "mL=" 52) where "x mL= y \<equiv> \<forall>(\<lambda>\<phi>. (\<phi> x m\<rightarrow> \<phi> y))"
+  abbreviation mbox :: "\<sigma> \<Rightarrow> \<sigma>" ("\<box>") where "\<box> \<phi> \<equiv> (\<lambda>w. \<forall>v.  w r v \<longrightarrow> \<phi> v)"
+  abbreviation mdia :: "\<sigma> \<Rightarrow> \<sigma>" ("\<diamond>") where "\<diamond> \<phi> \<equiv> (\<lambda>w. \<exists>v. w r v \<and> \<phi> v)" 
+
+  (*<*) no_syntax "_list" :: "args \<Rightarrow> 'a list" ("[(_)]") (*>*) 
+  abbreviation valid :: "\<sigma> \<Rightarrow> bool" ("[_]") where "[p] \<equiv> \<forall>w. p w"
+
+  
+section {* G\"odel's Ontological Argument *}  
+  
+text {* Constant symbol @{text "P"} (G\"odel's `Positive') is declared. *}
+
+
+  consts P :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>"  
+
+text {* The meaning of @{text "P"} is restricted by axioms @{text "A1(a/b)"}: $\all \phi 
+[P(\neg \phi) \biimp \neg P(\phi)]$ (Either a property or its negation is positive, but not both.) 
+and @{text "A2"}: $\all \phi \all \psi [(P(\phi) \wedge \nec \all x [\phi(x) \imp \psi(x)]) 
+\imp P(\psi)]$ (A property necessarily implied by a positive property is positive). *}
+
+  axiomatization where
+    A1a: "[\<forall>(\<lambda>\<Phi>. P (\<lambda>x. m\<not> (\<Phi> x)) m\<rightarrow> m\<not> (P \<Phi>))]" and
+    A1b: "[\<forall>(\<lambda>\<Phi>. m\<not> (P \<Phi>) m\<rightarrow> P (\<lambda>x. m\<not> (\<Phi> x)))]" and
+    A2:  "[\<forall>(\<lambda>\<Phi>. \<forall>(\<lambda>\<Psi>. (P \<Phi> m\<and> \<box> (\<forall>i(\<lambda>x. \<Phi> x m\<rightarrow> \<Psi> x))) m\<rightarrow> P \<Psi>))]"
+
+text {* We prove theorem T1: $\all \phi [P(\phi) \imp \pos \ex x \phi(x)]$ (Positive 
+properties are possibly exemplified). T1 is proved directly by Sledgehammer with command @{text 
+"sledgehammer [provers = remote_leo2]"}. 
+Sledgehammer suggests to call Metis with axioms A1a and A2. 
+Metis sucesfully generates a proof object 
+that is verified in Isabelle/HOL's kernel. *}
+ 
+  theorem T1: "[\<forall>(\<lambda>\<Phi>. P \<Phi> m\<rightarrow> \<diamond> (\<exists>i \<Phi>))]"  
+  -- {* sledgehammer [provers = remote\_leo2] *}
+  by (metis A1a A2)
+
+text {* Next, the symbol @{text "G"} for `God-like'  is introduced and defined 
+as $G(x) \biimp \forall \phi [P(\phi) \to \phi(x)]$ \\ (A God-like being possesses 
+all positive properties). *} 
+
+  definition G :: "\<mu> \<Rightarrow> \<sigma>" where "G = (\<lambda>x. \<forall>(\<lambda>\<Phi>. P \<Phi> m\<rightarrow> \<Phi> x))"   
+
+text {* Axiom @{text "A3"} is added: $P(G)$ (The property of being God-like is positive).
+Sledgehammer and Metis then prove corollary @{text "C"}: $\pos \ex x G(x)$ 
+(Possibly, God exists). *} 
+ 
+  axiomatization where A3:  "[P G]" 
+
+  corollary C: "[\<diamond> (\<exists>i G)]" 
+  -- {* sledgehammer [provers = remote\_leo2] *}
+  by (metis A3 T1)
+
+text {* Axiom @{text "A4"} is added: $\all \phi [P(\phi) \to \Box \; P(\phi)]$ 
+(Positive properties are necessarily positive). *}
+
+  axiomatization where A4:  "[\<forall>(\<lambda>\<Phi>. P \<Phi> m\<rightarrow> \<box> (P \<Phi>))]" 
+
+text {* Symbol @{text "ess"} for `Essence' is introduced and defined as 
+$$\ess{\phi}{x} \biimp \phi(x) \wedge \all \psi (\psi(x) \imp \nec \all y (\phi(y) 
+\imp \psi(y)))$$ (An \emph{essence} of an individual is a property possessed by it and necessarily implying any of its properties). *}
+
+  definition ess :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<mu> \<Rightarrow> \<sigma>" (infixr "ess" 85) where
+    "\<Phi> ess x = \<forall>(\<lambda>\<Psi>. \<Psi> x m\<rightarrow> \<box> (\<forall>i(\<lambda>y. \<Phi> y m\<rightarrow> \<Psi> y)))"
+
+text {* Next, Sledgehammer and Metis prove theorem @{text "T2"}: $\all x [G(x) \imp \ess{G}{x}]$ \\
+(Being God-like is an essence of any God-like being). *}
+
+  theorem T2: "[\<forall>i(\<lambda>x. G x m\<rightarrow> G ess x)]"
+  -- {* sledgehammer [provers = remote\_leo2] *}
+  by (metis A1b A4 G_def ess_def)
+
+text {* Symbol @{text "NE"}, for `Necessary Existence', is introduced and
+defined as $$\NE(x) \biimp \all \phi [\ess{\phi}{x} \imp \nec \ex y \phi(y)]$$ (Necessary 
+existence of an individual is the necessary exemplification of all its essences). *}
+
+  definition NE :: "\<mu> \<Rightarrow> \<sigma>" where "NE = (\<lambda>x. \<forall>(\<lambda>\<Phi>. \<Phi> ess x m\<rightarrow> \<box> (\<exists>i \<Phi>)))"
+
+text {* Moreover, axiom @{text "A5"} is added: $P(\NE)$ (Necessary existence is a positive 
+property). *}
+
+  axiomatization where A5:  "[P NE]"
+
+
+text {* The inconsistency of the entire theory is confirmed with the help of
+Leo2 and metis. *}
+
+
+text {* Lemma1 shows that the empty property is an essential property of each individual (if
+the first conjunct in the definition of ess is omitted). *}
+
+  lemma Lemma1: "[\<forall>i(\<lambda>x.((\<lambda>y.\<lambda>w.\<not> (y = y)) ess x))]"
+  -- {* sledgehammer [provers = remote\_leo2] *}
+  by (metis ess_def)
+
+text {* Lemma2 shows (using Lemma1) that NE becomes empty, i.e. NE is not exemplified. Too see this
+informally use the empty property to instantiate \<Phi> in the definition of NE. Actually,
+A5 can be replaced by a weaker assumption, namely that property P (positive) is exemplified. This 
+is illustrated by Lemma3 (Lemma3 has no other purpose here).*}
+
+  lemma  Lemma2: "[m\<not> (\<exists>i NE)]"
+  -- {* sledgehammer [provers = remote\_leo2] *}
+  by (metis A1a A2 A5 Lemma1 NE_def)
+
+  lemma  Lemma3: "[(\<exists>(\<lambda>\<Phi>. P \<Phi>)) m\<rightarrow> (m\<not> (\<exists>i NE))]"
+  -- {* sledgehammer [provers = remote\_leo2] *}
+  by (metis A1a A2 Lemma1 NE_def)
+
+text {* Now the inconsistency easily follows from Lemma2 T1 A5. *}
+  lemma False
+  -- {* sledgehammer [provers = remote\_leo2] *}
+  by (metis Lemma2 T1 A5)
+
+(*<*) 
+end
+(*>*) 
