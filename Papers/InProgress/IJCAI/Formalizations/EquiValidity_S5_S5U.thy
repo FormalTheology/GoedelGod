de6f6888d8a8444d3a96bffac486dd3f8ebad883
Benzmüller, Christoph <cbenzmueller@zedat.fu-berlin.de>
2016-01-15 09:36:39 -0800

    paper update

--- Papers/InProgress/IJCAI/Formalizations/EquiValidity_S5_S5U.thy
+++ Papers/InProgress/IJCAI/Formalizations/EquiValidity_S5_S5U.thy
@@ -6,11 +6,18 @@ begin
  abbreviation mdiaU :: "\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<diamond>u") 
   where "\<^bold>\<diamond>u \<phi> \<equiv> \<lambda>w.\<exists>v. \<phi>(v)" 
 
-  theorem test1:  "\<lfloor>(\<^bold>\<box>u \<phi>) \<^bold>\<rightarrow> (\<^bold>\<box> \<phi>)\<rfloor>"
+  theorem test1:  "(\<lfloor>\<^bold>\<box> \<phi>\<rfloor>) \<longrightarrow> (\<lfloor>\<^bold>\<box>u \<phi>\<rfloor>)"
+  sledgehammer
+  using ref by blast
+
+  theorem test2:  "(\<lfloor>\<^bold>\<box>u \<phi>\<rfloor>) \<longrightarrow> (\<lfloor>\<^bold>\<box> \<phi>\<rfloor>)"
   sledgehammer
   by simp
 
-  theorem test2:  "\<lfloor>(\<^bold>\<box> \<phi>) \<^bold>\<rightarrow> (\<^bold>\<box>u \<phi>)\<rfloor>"
+  theorem test3:  "\<lfloor>(\<^bold>\<box>u \<phi>) \<^bold>\<rightarrow> (\<^bold>\<box> \<phi>)\<rfloor>"
   sledgehammer
   by simp
+
+  theorem test4:  "\<lfloor>(\<^bold>\<box> \<phi>) \<^bold>\<rightarrow> (\<^bold>\<box>u \<phi>)\<rfloor>"
+  nitpick  [user_axioms,expect=genuine] oops
 end
