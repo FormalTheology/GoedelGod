3520a2f377cd3f031c51aee16f2cfb76b9fd1d3e
Bruno Woltzenlogel Paleo <bruno.wp@gmail.com>
2016-05-25 22:56:36 +1000

    Move IJCAI paper and formalizations

--- Papers/InProgress/IJCAI/Formalizations/BarcanFormulas_S5U.thy
+++
@@ -1,8 +0,0 @@
-theory BarcanFormulas_S5U imports QML_S5 
-begin
-  theorem BF:  "\<lfloor>(\<^bold>\<forall>x. \<^bold>\<box>(\<phi>(x))) \<^bold>\<rightarrow> \<^bold>\<box>(\<^bold>\<forall>x. \<phi>(x))\<rfloor>"
-  by simp
-
-  theorem CBF:  "\<lfloor>\<^bold>\<box>(\<^bold>\<forall>x. \<phi>(x)) \<^bold>\<rightarrow> (\<^bold>\<forall>x. \<^bold>\<box>(\<phi>(x)))\<rfloor>"
-  by simp
-end
