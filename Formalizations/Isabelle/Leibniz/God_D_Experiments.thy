ca1d6fb4040c4d8f6c65e3b94733dda8d2fea6c0
David Streit <david-streit@web.de>
2016-08-23 11:28:24 +0200

    Use Implication but screw up proofs

--- Formalizations/Isabelle/Leibniz/God_D_Experiments.thy
+++ Formalizations/Isabelle/Leibniz/God_D_Experiments.thy
@@ -1,28 +1,31 @@
 theory God_D_Experiments
-imports AoC
+imports AoC_Implication
 
 begin
 consts 
- E :: "c" ("E!")  
+ E :: "c" ("E")  
  G :: "c" ("G")
 
 definition N :: "c \<Rightarrow> bool" where "N A \<equiv> \<not> P (\<^bold>~ A) "
 
-context (* Short proof with axiom N((\<^bold>~G) \<^bold>\<or> E!) *)
+context (* Short proof with axiom "N(G \<^bold>\<longrightarrow> E)" *)
  assumes 
-  NG: "N((\<^bold>~G) \<^bold>\<or> E!)"
+  NG: "N(G \<^bold>\<longrightarrow> E)"
  begin
-  lemma L10: "(P G) \<longrightarrow> (G \<^bold>\<in> E)" 
-  by (metis CONJ3 NEG1 NEG2 NG N_def POSS1 POSS2 disjunction_def equal_def)
-  lemma L11: "(G \<^bold>\<in> E)" (* this shows that the assumption of possibility is not needed. *)
-  by (metis CONT4 IDEN2 NEG1 NG N_def POSS2 disjunction_def)
+  lemma L10: "(P G) \<longrightarrow> (G \<^bold>\<sqsupset> E)"
+  (*proof needed*)
 
-  (* this shows that necessity of concept implication reflects concept containment *)
-  lemma L12: "\<forall>X.\<forall>Y.( (N (disjunction (not X) Y) ) \<longrightarrow> (contains X Y))" 
-  by (metis CONT4 IDEN2 NEG1 N_def POSS2 disjunction_def)
+(* This shows that the assumption of possibility is not needed. *)
+  lemma L11: "(G \<^bold>\<sqsupset> E)" 
+(*Proof needed*)
 
-  lemma L13: "\<forall>X.\<forall>Y.( (contains X Y)) \<longrightarrow>  (N (disjunction (not X) Y) )" 
-  by (metis CONT4 IDEN2 NEG1 N_def POSS2 disjunction_def) 
+  (* This shows that necessity of concept implication reflects concept containment *)
+  lemma L12: "\<forall>X.\<forall>Y.( (N (Y \<^bold>\<longrightarrow> Y) ) \<longrightarrow> ( X \<^bold>\<sqsupset> Y))" 
+(*Proof needed*)
+
+  lemma L13: "\<forall>X.\<forall>Y.( (X \<^bold>\<sqsupset> Y)) \<longrightarrow>  (N (Y \<^bold>\<longrightarrow> Y) )" sledgehammer
+    by (smt CONT2 DISJ1 DISJ2 NEG2 NEG5 N_def implication_def notcontains_def)
+
  
  end
 
@@ -30,8 +33,9 @@ context (* Short proof attempt with axiom (N G) *)
  assumes 
   NG: "N G"
  begin
-  lemma L10': "(P G) \<longrightarrow> (G \<^bold>\<in> E)" 
-  nitpick [user_axioms, show_all, format = 2] sorry
+  lemma L10': "(P G) \<longrightarrow> (G \<^bold>\<sqsupset> E)" 
+    nitpick[user_axioms, show_all, format = 2] oops
+
  end
 
 
