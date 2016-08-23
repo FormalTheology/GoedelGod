122e32cbf9ccf48e0141255a899ac75d6f95784f
Benzmüller, Christoph <cbenzmueller@zedat.fu-berlin.de>
2016-08-23 17:21:24 +0200

    updates; figures added

--- Formalizations/Isabelle/Leibniz/God_D_Experiments.thy
+++ Formalizations/Isabelle/Leibniz/God_D_Experiments.thy
@@ -22,13 +22,9 @@ context (* Short proof with axiom "N(G \<^bold>\<longrightarrow> E)" *)
   lemma L11: "(G \<^bold>\<sqsupset> E)" 
     by (metis CONT2 NEG1 NG N_def POSS1 POSS2 disjunction_def equal_def implication_def)
 
-  (* Necessity of concept implication and concept containment *)
-  lemma L12: "\<forall>X.\<forall>Y.(N(Y \<^bold>\<longrightarrow> Y) \<longrightarrow> (X \<^bold>\<sqsupset> Y))"
-    nitpick [user_axioms, show_all, format = 2] sorry  --{* Countermodel *}
-  lemma L13: "\<forall>X.\<forall>Y.(X \<^bold>\<sqsupset> Y) \<longrightarrow>  (N(Y \<^bold>\<longrightarrow> Y) )"
-    by (smt CONT2 DISJ1 DISJ2 NEG2 NEG5 N_def implication_def notcontains_def)
-
- 
+  (* Necessity of concept implication is equivalent to concept containment *)
+  lemma L12: "\<forall>X.\<forall>Y.(N(X \<^bold>\<longrightarrow> Y) \<longleftrightarrow> (X \<^bold>\<sqsupset> Y))"
+    by (metis CONT2 NEG1 N_def POSS1 POSS2 disjunction_def equal_def implication_def)
  end
 
 context (* Short proof attempt with axiom (N G) *)
@@ -37,7 +33,6 @@ context (* Short proof attempt with axiom (N G) *)
  begin
   lemma L10': "(P G) \<longrightarrow> (G \<^bold>\<sqsupset> E)" 
     nitpick[user_axioms, show_all, format = 2] oops
-
  end
 
 
