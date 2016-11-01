ad13ccce98a45d5a48a6b137db0ad64196f2224b
Benzmüller, Christoph <cbenzmueller@zedat.fu-berlin.de>
2016-11-01 09:48:05 +0100

    varying domain variant added

--- Formalizations/Isabelle/ScottLocal/Scott_Local_K4B.thy
+++ Formalizations/Isabelle/ScottLocal/Scott_Local_K4B.thy
@@ -1,48 +1,6 @@
-theory Scott_Local_K4B imports Main 
+theory Scott_Local_K4B imports QML_Local_Const
 begin
  declare [[smt_solver = cvc4]]
-
-  typedecl i -- "type for possible worlds" 
-  typedecl \<mu> -- "type for individuals"      
-  consts r :: "i\<Rightarrow>i \<Rightarrow>bool" (infixr"r"70)     
-             -- "accessibility relation r"   
-  type_synonym \<sigma> = "(i\<Rightarrow>bool)"
-
- abbreviation mneg :: "\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<not>_"[52]53) 
-  where "\<^bold>\<not>\<phi> \<equiv> \<lambda>w. \<not>\<phi>(w)" 
- abbreviation mnegpred :: "(\<mu>\<Rightarrow>\<sigma>)\<Rightarrow>(\<mu>\<Rightarrow>\<sigma>)" ("\<^sup>\<not>_"[52]53) 
-  where "\<^sup>\<not>\<Phi> \<equiv> \<lambda>x.\<lambda>w. \<not>\<Phi>(x)(w)"  
- abbreviation mand :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" (infixr"\<^bold>\<and>"51) 
-  where "\<phi>\<^bold>\<and>\<psi> \<equiv> \<lambda>w. \<phi>(w)\<and>\<psi>(w)"   
- abbreviation mor  :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" (infixr"\<^bold>\<or>"50) 
-  where "\<phi>\<^bold>\<or>\<psi> \<equiv> \<lambda>w. \<phi>(w)\<or>\<psi>(w)"   
- abbreviation mimpl :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" (infixr"\<^bold>\<rightarrow>"49) 
-  where "\<phi>\<^bold>\<rightarrow>\<psi> \<equiv> \<lambda>w. \<phi>(w)\<longrightarrow>\<psi>(w)"  
- abbreviation mequiv :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" (infixr"\<^bold>\<equiv>"48) 
-  where "\<phi>\<^bold>\<equiv>\<psi> \<equiv> \<lambda>w. \<phi>(w)\<longleftrightarrow>\<psi>(w)"  
- abbreviation mall :: "('a\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>" ("\<^bold>\<forall>") 
-  where "\<^bold>\<forall>\<Phi> \<equiv> \<lambda>w.\<forall>x. \<Phi>(x)(w)"
- abbreviation mallB:: "('a\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>" (binder"\<^bold>\<forall>"[8]9) 
-  where "\<^bold>\<forall>x. \<phi>(x) \<equiv> \<^bold>\<forall>\<phi>"   
- abbreviation mexi :: "('a\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>" ("\<^bold>\<exists>") 
-  where "\<^bold>\<exists>\<Phi> \<equiv> \<lambda>w.\<exists>x. \<Phi>(x)(w)"
- abbreviation mexiB:: "('a\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>" (binder"\<^bold>\<exists>"[8]9) 
-  where "\<^bold>\<exists>x. \<phi>(x) \<equiv> \<^bold>\<exists>\<phi>"   
- abbreviation mbox :: "\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<box>") 
-  where "\<^bold>\<box>\<phi> \<equiv> \<lambda>w. \<forall>v.  w r v \<longrightarrow> \<phi> v"
- abbreviation mdia :: "\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<diamond>") 
-  where "\<^bold>\<diamond>\<phi> \<equiv> \<lambda>w. \<exists>v. w r v \<and> \<phi> v" 
-
- abbreviation valid :: "\<sigma> \<Rightarrow> bool" ("\<lfloor>_\<rfloor>"[7]8) 
-  where "\<lfloor>p\<rfloor> \<equiv> \<forall>w. p w"
-
-(* Local validity of a (higher-order) modal logic formula means truth
-   with respect to the current world *)
- consts cw :: i 
-
- abbreviation mlocalvalid :: "\<sigma> \<Rightarrow> bool" ("\<lfloor>_\<rfloor>\<^sup>c\<^sup>w"[7]8) 
-  where "\<lfloor>p\<rfloor>\<^sup>c\<^sup>w \<equiv> p cw"
-
 
   consts P :: "(\<mu>\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>"                                     (* Positive *)
 
@@ -121,7 +79,7 @@ qed
 lemma MC_pre: "\<lfloor>\<Phi> \<^bold>\<rightarrow> (\<^bold>\<box> \<Phi>)\<rfloor>\<^sup>c\<^sup>w"  
   proof-
    have L1: "\<lfloor>\<^bold>\<forall>z. G(z) \<^bold>\<rightarrow> (\<^bold>\<forall>\<psi>. (\<psi>(z) \<^bold>\<rightarrow> \<^bold>\<box>(\<^bold>\<forall>x. G(x) \<^bold>\<rightarrow> \<psi>(x))))\<rfloor>\<^sup>c\<^sup>w" by (smt T2 ess_def)
-   hence L2: "\<lfloor>\<^bold>\<forall>z. G(z) \<^bold>\<rightarrow> (\<Phi> \<^bold>\<rightarrow> \<^bold>\<box>(\<^bold>\<forall>x. G(x) \<^bold>\<rightarrow> \<Phi>))\<rfloor>\<^sup>c\<^sup>w" by meson
+   hence L2: "\<lfloor>\<^bold>\<forall>z. G(z) \<^bold>\<rightarrow> (\<Phi> \<^bold>\<rightarrow> \<^bold>\<box>(\<^bold>\<forall>z. G(z) \<^bold>\<rightarrow> \<Phi>))\<rfloor>\<^sup>c\<^sup>w" by meson
    thus ?thesis by (meson sym trans T3)
   qed
 
