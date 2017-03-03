6c75a75a153fd93f293fa15e80516cf7c1c150a3
David Streit <david-streit@web.de>
2017-03-03 12:09:51 +0100

    Add Caramuel's proof and show Inconsistency wir VarDom

---
+++ Formalizations/Isabelle/Caramuel-Hajek/InconsistencyVar.thy
@@ -0,0 +1,24 @@
+theory InconsistencyVar
+  
+imports Main QML_S5_var
+    
+begin
+text {*
+This is a quick demonstration that if using the QML_S5_var.thy which states that 
+"\<forall>w. \<exists>x. eiw x w" Caramuels Assumptions become inconsistent.*}
+  
+consts G:: "\<mu> \<Rightarrow> \<sigma>" (*Property of being godlike*)  
+consts P:: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>"  (*(2. order) property of being positive*)
+abbreviation E :: "\<mu> \<Rightarrow> \<sigma>" ("\<^bold>\<forall>\<^sup>E") where "E \<equiv> eiw" (*For consistency with the paper\<acute>s notation*)
+
+
+theorem Inconsistent: (*Uses the assumptions by Hajek\<acute>s Caramuel*)
+assumes  *: "\<And>u.\<lfloor>(G u) \<^bold>\<rightarrow> \<^bold>\<box>(G u \<^bold>\<and> E u) \<rfloor>"
+and Ax4: "\<lfloor>\<^bold>\<diamond> (\<^bold>\<forall>x. (\<^bold>\<not> E x  )) \<rfloor>" 
+shows "A \<and> \<not>A" 
+proof -
+  have False using Ax4 nonempty by blast
+  thus ?thesis by blast
+qed
+end
+  
