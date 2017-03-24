c95f256aaed7b8863cda3479bbeb63e468aa8cdc
David Streit <david-streit@web.de>
2017-03-24 10:37:05 +0100

    Cleanup Misc

--- Formalizations/Isabelle/Misc/ATP_Performance.thy
+++ Formalizations/Isabelle/Misc/ATP_Performance.thy
@@ -9,34 +9,34 @@ section {* Interesting Problems for Understanding and Comparing ATP Performance 
   consts P :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>"  
 
   definition G1 :: "\<mu> \<Rightarrow> \<sigma>" where 
-            "G1 = (\<lambda>x. \<^bold>\<forall>(\<lambda>\<Phi>. (\<^bold>\<box> (\<Phi> x ))  \<^bold>\<leftrightarrow>  ( \<^bold>\<exists>(\<lambda>\<Psi>.( (P \<Psi>) \<^bold>\<and> (\<^bold>\<box> (\<^bold>\<forall>\<^sup>E(\<lambda>x. \<Psi> x \<^bold>\<rightarrow> \<Phi> x))) )))  ))" 
+            "G1 x \<equiv> \<^bold>\<forall>\<Phi>.( (\<^bold>\<box> (\<Phi> x ))  \<^bold>\<leftrightarrow>  ( \<^bold>\<exists>\<Psi>.(( (P \<Psi>) \<^bold>\<and> (\<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.( \<Psi> x \<^bold>\<rightarrow> \<Phi> x))) )) ))" 
 
   axiomatization where A3_1:  "\<lfloor>P G1\<rfloor>" 
 
-  lemma L1_1: "\<lfloor>\<^bold>\<forall>\<^sup>E(\<lambda>u.( (G1 u) \<^bold>\<rightarrow> (\<^bold>\<box> (G1 u) ) ) ) \<rfloor>"
-  sledgehammer [remote_satallax, verbose, timeout = 60] (G1_def A3_1)
-  sledgehammer [remote_leo2, verbose, timeout = 60] (G1_def A3_1)
+  lemma L1_1: "\<lfloor>\<^bold>\<forall>\<^sup>Eu.(( (G1 u) \<^bold>\<rightarrow> (\<^bold>\<box> (G1 u) ) ) ) \<rfloor>"
+(*  sledgehammer [remote_satallax, verbose, timeout = 60] (G1_def A3_1)
+  sledgehammer [remote_leo2, verbose, timeout = 60] (G1_def A3_1)*)
   by (metis (erased, lifting) A3_1 G1_def)
 
   definition P' :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" where
-            "P' = (\<lambda>\<Phi>. \<^bold>\<exists>(\<lambda>\<Psi>.((P \<Psi>) \<^bold>\<and> (\<^bold>\<box> (\<^bold>\<forall>\<^sup>E(\<lambda>x.((\<Psi> x) \<^bold>\<rightarrow> (\<Phi> x) ) )))  ) ))     "
+            "P' \<Phi> \<equiv> \<^bold>\<exists>\<Psi>.(((P \<Psi>) \<^bold>\<and> (\<^bold>\<box> (\<^bold>\<forall>\<^sup>Ex.(((\<Psi> x) \<^bold>\<rightarrow> (\<Phi> x) ) )))))"
 
   definition G2 :: "\<mu> \<Rightarrow> \<sigma>" where 
-            "G2 = (\<lambda>x. \<^bold>\<forall>(\<lambda>\<Phi>. ( (\<^bold>\<box> (\<Phi> x ))) \<^bold>\<leftrightarrow> P' \<Phi> ))" 
+            "G2 x \<equiv> \<^bold>\<forall>\<Phi>.( ( (\<^bold>\<box> (\<Phi> x ))) \<^bold>\<leftrightarrow> P' \<Phi> )" 
 
-  axiomatization where A3_2:  "\<lfloor>P (\<lambda>x. (G2 x) ) \<rfloor>" 
+  axiomatization where A3_2:  "\<lfloor>P G2\<rfloor>" 
 
-  lemma L1_2: "\<lfloor>\<^bold>\<forall>\<^sup>E(\<lambda>u.( (G2 u) \<^bold>\<rightarrow> (\<^bold>\<box> (G2 u) ) ) ) \<rfloor>"
-  sledgehammer [remote_satallax, verbose, timeout = 60] (G2_def A3_2 P'_def)
-  sledgehammer [remote_leo2, verbose, timeout = 60] (G2_def A3_2 P'_def)
+  lemma L1_2: "\<lfloor>\<^bold>\<forall>\<^sup>Eu.((G2 u) \<^bold>\<rightarrow> (\<^bold>\<box> (G2 u) ) )\<rfloor>"
+(*  sledgehammer [remote_satallax, verbose, timeout = 60] (G2_def A3_2 P'_def)
+  sledgehammer [remote_leo2, verbose, timeout = 60] (G2_def A3_2 P'_def)*)
   by (metis (erased, lifting) A3_2 G2_def P'_def)
 
 
-  axiomatization where A3_3:  "\<lfloor>P (\<lambda>x. (G1 x) ) \<rfloor>" 
+  axiomatization where A3_3:  "\<lfloor>P G1\<rfloor>" 
 
-  lemma L1_3: "\<lfloor>\<^bold>\<forall>\<^sup>E(\<lambda>u.( (G1 u) \<^bold>\<rightarrow> (\<^bold>\<box> (G1 u) ) ) ) \<rfloor>"
-  sledgehammer [remote_satallax, verbose, timeout = 60] (G1_def A3_3)
-  sledgehammer [remote_leo2, verbose, timeout = 60] (G1_def A3_3)
+  lemma L1_3: "\<lfloor>\<^bold>\<forall>\<^sup>Eu.(( (G1 u) \<^bold>\<rightarrow> (\<^bold>\<box> (G1 u) ) ) ) \<rfloor>"
+(*  sledgehammer [remote_satallax, verbose, timeout = 60] (G1_def A3_3)
+  sledgehammer [remote_leo2, verbose, timeout = 60] (G1_def A3_3)*)
   by (metis (erased, lifting) G1_def A3_3)
 
 end
--- Formalizations/Isabelle/Misc/Inconsistency.thy
+++ Formalizations/Isabelle/Misc/Inconsistency.thy
@@ -4,11 +4,11 @@ imports Main "../QML"
 begin
 section {* Investigation of Goedel's Inconsistency *}
 
-  abbreviation mFalse :: "\<sigma>" where "mFalse \<equiv> (\<lambda>w. (\<^bold>\<exists>(\<lambda>p. p \<^bold>\<and> \<^bold>\<not> p)) w)"
+  abbreviation mFalse :: "\<sigma>" where "mFalse \<equiv> (\<lambda>w. (\<^bold>\<exists>p.( p \<^bold>\<and> \<^bold>\<not> p)) w)"
 
   lemma dia_box_false_leads_to_false: "\<lfloor>(\<^bold>\<diamond> (\<^bold>\<box> mFalse))\<rfloor> \<longrightarrow> \<lfloor>mFalse\<rfloor>"
-  sledgehammer [provers = remote_leo2]
-  nitpick [satisfy]
+(*  sledgehammer [provers = remote_leo2]
+  nitpick [satisfy]*)
   by meson 
 
   lemma dia_box_false_implies_false: "\<lfloor>(\<^bold>\<diamond>   (\<^bold>\<box> mFalse)) \<^bold>\<rightarrow> mFalse\<rfloor>"
