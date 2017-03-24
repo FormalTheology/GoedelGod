35f3dfc9506e2231575ad822be72a3a50d224c41
David Streit <david-streit@web.de>
2017-03-24 10:34:19 +0100

    Cleanup GoedelS5

--- Formalizations/Isabelle/GoedelS5/GoedelGodWithoutConjunctInEss.thy
+++ Formalizations/Isabelle/GoedelS5/GoedelGodWithoutConjunctInEss.thy
@@ -3,29 +3,29 @@ imports  Main "../QML_S5"
 
 begin
   consts P :: "(\<mu> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>"  
-  definition ess (infixr "ess" 85) where "\<Phi> ess x = \<^bold>\<forall>(\<lambda>\<Psi>. \<Psi> x \<^bold>\<rightarrow> \<^bold>\<box> (\<^bold>\<forall>(\<lambda>y. \<Phi> y \<^bold>\<rightarrow> \<Psi> y)))"
-  definition NE  where                   "NE x    = \<^bold>\<forall>(\<lambda>\<Phi>. \<Phi> ess x \<^bold>\<rightarrow> \<^bold>\<box> (\<^bold>\<exists> \<Phi>))"
-  axiomatization where A1a: "\<lfloor>\<^bold>\<forall>(\<lambda>\<Phi>. P (\<lambda>x. \<^bold>\<not> (\<Phi> x)) \<^bold>\<rightarrow> \<^bold>\<not> (P \<Phi>))\<rfloor>"
-                   and A2:  "\<lfloor>\<^bold>\<forall>(\<lambda>\<Phi>. \<^bold>\<forall>(\<lambda>\<Psi>. (P \<Phi> \<^bold>\<and> \<^bold>\<box> (\<^bold>\<forall>(\<lambda>x. \<Phi> x \<^bold>\<rightarrow> \<Psi> x))) \<^bold>\<rightarrow> P \<Psi>))\<rfloor>" 
+  definition ess (infixr "ess" 85) where "\<Phi> ess x \<equiv> \<^bold>\<forall>\<Psi>.( \<Psi> x \<^bold>\<rightarrow> \<^bold>\<box> (\<^bold>\<forall>y.( \<Phi> y \<^bold>\<rightarrow> \<Psi> y)))"
+  definition NE  where                   "NE x    \<equiv> \<^bold>\<forall>\<Phi>.( \<Phi> ess x \<^bold>\<rightarrow> \<^bold>\<box> (\<^bold>\<exists>x. (\<Phi> x)))"
+  axiomatization where A1a: "\<lfloor>\<^bold>\<forall>\<Phi>.( P (\<lambda>x. \<^bold>\<not> (\<Phi> x)) \<^bold>\<rightarrow> \<^bold>\<not> (P \<Phi>))\<rfloor>"
+                   and A2:  "\<lfloor>\<^bold>\<forall>\<Phi>.( \<^bold>\<forall>\<Psi>. ((P \<Phi> \<^bold>\<and> \<^bold>\<box> (\<^bold>\<forall>x.( \<Phi> x \<^bold>\<rightarrow> \<Psi> x))) \<^bold>\<rightarrow> P \<Psi>))\<rfloor>" 
 
 -- {* Positive properties are possibly exemplified. *}
-  theorem T1: "\<lfloor>\<^bold>\<forall>(\<lambda>\<Phi>. P \<Phi> \<^bold>\<rightarrow> \<^bold>\<diamond> (\<^bold>\<exists> \<Phi>))\<rfloor>"                                
+  theorem T1: "\<lfloor>\<^bold>\<forall>\<Phi>.( P \<Phi> \<^bold>\<rightarrow> \<^bold>\<diamond> (\<^bold>\<exists>x. \<Phi> x))\<rfloor>"                                
   by (metis A1a A2)
 
 -- {* The empty property is an essence of every individual. *}
-  lemma Lemma1: "\<lfloor>(\<^bold>\<forall>(\<lambda>x.((\<lambda>y.\<lambda>w. False) ess x)))\<rfloor>"                       
+  lemma Lemma1: "\<lfloor>(\<^bold>\<forall>x.(((\<lambda>y.\<lambda>w. False) ess x)))\<rfloor>"                       
   by (metis ess_def)
 
   axiomatization where A5:  "\<lfloor>P NE\<rfloor>"
 
 -- {* Now the inconsistency follows from A5, Lemma1, NE_def and T1 *}
   lemma False          
-  sledgehammer [remote_leo2 remote_satallax] (A1a A2 A5 NE_def)
-  sledgehammer [remote_leo2 remote_satallax] (A1a A2 A5 Lemma1 NE_def)
+  (*sledgehammer [remote_leo2 remote_satallax] (A1a A2 A5 NE_def)
+  sledgehammer [remote_leo2 remote_satallax] (A1a A2 A5 Lemma1 NE_def) *)
   by (metis A1a A2 A5 Lemma1 NE_def)
   -- {* by (metis A5 NE_def T1) *}
 
 
-  by (metis A5 Lemma1 NE_def T1) 
+
 end   
  
